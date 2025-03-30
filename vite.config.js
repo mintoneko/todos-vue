acdb49785bc0d4df496dbf718c7f6b4a4e7668fc
mintoneko <mobaisilent@gmail.com>
2025-03-30 11:03:39 +0800

    configure environment variables to solve URL path problem

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: process.env.VITE_BASE_PUBLIC || '/', // 新增base配置
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
